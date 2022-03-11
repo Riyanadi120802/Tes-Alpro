7f649f568e9fce233b8d4935717903bb2b885ae1
Anang <tyoandini@gmail.com>
2022-03-11 15:03:35 +0700

    update proses input dan file

--- input.h
+++ input.h
@@ -11,9 +11,49 @@ class DataParkir {
 			tulis.open("data/nama.txt");
 			tulis << this->nama << endl;
 			tulis.close();
+
+			tulis.open("data/kota.txt");
+			tulis << this->kota << endl;
+			tulis.close();
+
+			tulis.open("data/nama_kota.txt");
+			if (this->kota == "AD")
+				tulis << "Solo\n";
+			else if (this->kota == "F")
+				tulis << "Bogor\n";
+			else if (this->kota == "AB")
+				tulis << "Jogja\n";
+			tulis.close();
+
+			tulis.open("data/noplat.txt");
+			tulis << this->noplat << endl;
+			tulis.close();
+
+			tulis.open("data/ekstensi.txt");
+			tulis << this->ekstensi << endl;
+			tulis.close();
+
+			tulis.open("data/jenisKendaraan.txt");
+			if (this->jenisKendaraan == 1)
+				tulis << "Mobil\n";
+			else if (this->jenisKendaraan == 2)
+				tulis << "Motor\n";
+			tulis.close();
+
+
+			tulis.open("data/waktu.txt");
+			if (this->waktu - (int(this->waktu)) == 0)
+				tulis << this->waktu << endl;
+			else
+				tulis << int(this->waktu) + 1 << endl;
+			tulis.close();
+
+
+
+
 		}
 	private :
-		string nama, kota, noplat;
+		string nama, kota, noplat, nama_kota;
 		string ekstensi;
 		int jenisKendaraan;
 		float waktu;		
@@ -25,7 +65,8 @@ DataParkir::DataParkir(){
 	cout << "Masukkan nama       : "; 
 	getline(cin, nama);
 
-	cout << "Masukkan plat nomor : ";
+
+	cout << "Masukkan plat nomor (huruf besar) : ";
 	cin >> kota >> noplat >> ekstensi;
 	cout << "1) Mobil\n";
 	cout << "2) Motor\n";
